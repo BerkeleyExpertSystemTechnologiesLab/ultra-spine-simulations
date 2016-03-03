f85ca8429c8770a42bf83b79d44bc0e2f56ead39
Abishek <abishek.akella@gmail.com>
2016-03-02 23:32:22 -0800

    Updated explanations for simulate_dynamics

--- control/mpc/simulate_dynamics.m
+++ control/mpc/simulate_dynamics.m
@@ -9,9 +9,23 @@ function systemStates = simulate_dynamics(systemStates, restLengths, inputs, dt,
 % Needs to be manually modified when more links are being added - functions
 % getTension and duct_accel require state inputs from all links
 %
-% This script uses Runge-Kutta. The 'K' constants are as per that method.
 
-stateX{1} = 0; % First link is fixed, not important
+% This script uses the Runge-Kutta (RK4) method for temporal discretization
+% in order to forward simulate the system's dynamics. The method calculates
+% the rate of change of states using the continuous-time dynamics at
+% the start, end, and midpoint of each time step and uses the weighted sum to
+% approximate the system states at the next time instant. Incorporating
+% these additional terms minimizes truncation error relative to the
+% first-order solution (Euler's method).
+% 
+% K1 = f(t, systemStates{t})
+% K2 = f(t + dt/2, systemStates{t} + K1*dt/2)
+% K3 = f(t + dt/2, systemStates{t} + K2*dt/2)
+% K4 = f(t + dt, systemStates{t} + K3*dt)
+%
+% systemStates{t+1} = systemStates{t} + (dt/6)*(K1 + 2*K2 + 2*K3 + K4)
+
+stateX{1} = 0; % First link is fixed (Placeholder for state info)
 Te{1} = 0;
 initial_states = [];
 for k = 2:(links+1)
@@ -20,8 +34,10 @@ for k = 2:(links+1)
 end
 
 % Runge-Kutta step 1
-% For the current state, get all the forces on the rigid bodies (via
-% tensions)
+% Calculating the rate of change for each state in the system. These
+% correspond to linear/angular velocities for the first six states and
+% their corresponding accelerations for the second six states.
+
 for k = 2:(links+1)
     Te{k} = getTensions(stateX{2}(1),stateX{2}(2),stateX{2}(3),stateX{2}(4),stateX{2}(5),stateX{2}(6),stateX{2}(7),stateX{2}(8),stateX{2}(9),stateX{2}(10),stateX{2}(11),stateX{2}(12), ...
         stateX{3}(1),stateX{3}(2),stateX{3}(3),stateX{3}(4),stateX{3}(5),stateX{3}(6),stateX{3}(7),stateX{3}(8),stateX{3}(9),stateX{3}(10),stateX{3}(11),stateX{3}(12), ...
@@ -30,17 +46,19 @@ for k = 2:(links+1)
         restLengths(1), restLengths(2), restLengths(3), restLengths(4), restLengths(5), restLengths(6), restLengths(7), restLengths(8), k);
 end
 
-% Calculate the accelerations applied to the rigid bodies (this is where
-% the dynamics are called)
+% Duct_accel solves for the six accelerations for X, Y, Z and orientation
+% variables (Tau, Gamma, Phi). Since the velocities are already encoded in
+% the state information (final six states of 12-state vector), K1 is
+% subsequently built from the last six states of the state vector and these
+% six acceleration components.
+
 K = duct_accel(stateX{2}(1),stateX{2}(2),stateX{2}(3),stateX{2}(4),stateX{2}(5),stateX{2}(6),stateX{2}(7),stateX{2}(8),stateX{2}(9),stateX{2}(10),stateX{2}(11),stateX{2}(12),Te{2}(1),Te{2}(2),Te{2}(3),Te{2}(4),Te{2}(5),Te{2}(6),Te{2}(7),Te{2}(8), ...
     stateX{3}(1),stateX{3}(2),stateX{3}(3),stateX{3}(4),stateX{3}(5),stateX{3}(6),stateX{3}(7),stateX{3}(8),stateX{3}(9),stateX{3}(10),stateX{3}(11),stateX{3}(12),Te{3}(1),Te{3}(2),Te{3}(3),Te{3}(4),Te{3}(5),Te{3}(6),Te{3}(7),Te{3}(8), ...
     stateX{4}(1),stateX{4}(2),stateX{4}(3),stateX{4}(4),stateX{4}(5),stateX{4}(6),stateX{4}(7),stateX{4}(8),stateX{4}(9),stateX{4}(10),stateX{4}(11),stateX{4}(12),Te{4}(1),Te{4}(2),Te{4}(3),Te{4}(4),Te{4}(5),Te{4}(6),Te{4}(7),Te{4}(8));
 
-% Reformat the vectors to incorporate the velocities and accelerations into
-% one
 K1 = zeros(1, links*12);
 for k = 2:(links+1)
-    kn = 12*(k-2)+1;
+    kn = 12*(k-2)+1; % Indexing
     kk = 6*(k-2)+1;
     K1(kn:(kn+5)) = stateX{k}(7:12);
     K1((kn+6):(kn+11)) = K(kk:(kk+5));
@@ -48,8 +66,10 @@ end
 
 
 % Runge-Kutta step 2
-% For the current state, get all the forces on the rigid bodies (via
-% tensions)
+% Again, solving for rate of change for each state. This time the
+% calculation takes place at the midpoint of the interval and uses the
+% first-order estimate of the states at this point (systemStates + K1*dt/2).
+
 for k = 2:(links+1)
     Te{k} = getTensions(stateX{2}(1),stateX{2}(2),stateX{2}(3),stateX{2}(4),stateX{2}(5),stateX{2}(6),stateX{2}(7),stateX{2}(8),stateX{2}(9),stateX{2}(10),stateX{2}(11),stateX{2}(12), ...
         stateX{3}(1),stateX{3}(2),stateX{3}(3),stateX{3}(4),stateX{3}(5),stateX{3}(6),stateX{3}(7),stateX{3}(8),stateX{3}(9),stateX{3}(10),stateX{3}(11),stateX{3}(12), ...
@@ -58,14 +78,17 @@ for k = 2:(links+1)
         restLengths(1), restLengths(2), restLengths(3), restLengths(4), restLengths(5), restLengths(6), restLengths(7), restLengths(8), k);
 end
 
-% Apply the accelerations to the current state
 for k = 2:(links+1)
     n = 12*(k-2)+1;
     stateX{k} = stateX{k} + (K1(n:(11+n))*dt/2);
 end
 
-% Calculate the accelerations applied to the rigid bodies (this is where
-% the dynamics are called)
+% Duct_accel solves for the six accelerations for X, Y, Z and orientation
+% variables (Tau, Gamma, Phi). Since the velocities are already encoded in
+% the state information (final six states of 12-state vector), K2 is
+% subsequently built from the last six states of the state vector and these
+% six acceleration components.
+
 K = duct_accel(stateX{2}(1),stateX{2}(2),stateX{2}(3),stateX{2}(4),stateX{2}(5),stateX{2}(6),stateX{2}(7),stateX{2}(8),stateX{2}(9),stateX{2}(10),stateX{2}(11),stateX{2}(12),Te{2}(1),Te{2}(2),Te{2}(3),Te{2}(4),Te{2}(5),Te{2}(6),Te{2}(7),Te{2}(8), ...
     stateX{3}(1),stateX{3}(2),stateX{3}(3),stateX{3}(4),stateX{3}(5),stateX{3}(6),stateX{3}(7),stateX{3}(8),stateX{3}(9),stateX{3}(10),stateX{3}(11),stateX{3}(12),Te{3}(1),Te{3}(2),Te{3}(3),Te{3}(4),Te{3}(5),Te{3}(6),Te{3}(7),Te{3}(8), ...
     stateX{4}(1),stateX{4}(2),stateX{4}(3),stateX{4}(4),stateX{4}(5),stateX{4}(6),stateX{4}(7),stateX{4}(8),stateX{4}(9),stateX{4}(10),stateX{4}(11),stateX{4}(12),Te{4}(1),Te{4}(2),Te{4}(3),Te{4}(4),Te{4}(5),Te{4}(6),Te{4}(7),Te{4}(8));
@@ -77,7 +100,12 @@ for k = 2:(links+1)
     K2(kn:(kn+5)) = stateX{k}(7:12);
     K2((kn+6):(kn+11)) = K(kk:(kk+5));
 end 
-%
+
+% Runge-Kutta step 3
+% Again, solving for rate of change for each state. Once again the
+% calculation takes place at the midpoint of the interval and uses the
+% first-order estimate of the states at this point (systemStates + K2*dt/2).
+
 for k = 2:(links+1)
     Te{k} = getTensions(stateX{2}(1),stateX{2}(2),stateX{2}(3),stateX{2}(4),stateX{2}(5),stateX{2}(6),stateX{2}(7),stateX{2}(8),stateX{2}(9),stateX{2}(10),stateX{2}(11),stateX{2}(12), ...
         stateX{3}(1),stateX{3}(2),stateX{3}(3),stateX{3}(4),stateX{3}(5),stateX{3}(6),stateX{3}(7),stateX{3}(8),stateX{3}(9),stateX{3}(10),stateX{3}(11),stateX{3}(12), ...
@@ -91,6 +119,12 @@ for k = 2:(links+1)
     stateX{k} = stateX{k} + (K2(n:(11+n))*dt/2);
 end
 
+% Duct_accel solves for the six accelerations for X, Y, Z and orientation
+% variables (Tau, Gamma, Phi). Since the velocities are already encoded in
+% the state information (final six states of 12-state vector), K3 is
+% subsequently built from the last six states of the state vector and these
+% six acceleration components.
+
 K = duct_accel(stateX{2}(1),stateX{2}(2),stateX{2}(3),stateX{2}(4),stateX{2}(5),stateX{2}(6),stateX{2}(7),stateX{2}(8),stateX{2}(9),stateX{2}(10),stateX{2}(11),stateX{2}(12),Te{2}(1),Te{2}(2),Te{2}(3),Te{2}(4),Te{2}(5),Te{2}(6),Te{2}(7),Te{2}(8), ...
     stateX{3}(1),stateX{3}(2),stateX{3}(3),stateX{3}(4),stateX{3}(5),stateX{3}(6),stateX{3}(7),stateX{3}(8),stateX{3}(9),stateX{3}(10),stateX{3}(11),stateX{3}(12),Te{3}(1),Te{3}(2),Te{3}(3),Te{3}(4),Te{3}(5),Te{3}(6),Te{3}(7),Te{3}(8), ...
     stateX{4}(1),stateX{4}(2),stateX{4}(3),stateX{4}(4),stateX{4}(5),stateX{4}(6),stateX{4}(7),stateX{4}(8),stateX{4}(9),stateX{4}(10),stateX{4}(11),stateX{4}(12),Te{4}(1),Te{4}(2),Te{4}(3),Te{4}(4),Te{4}(5),Te{4}(6),Te{4}(7),Te{4}(8));
@@ -102,6 +136,11 @@ for k = 2:(links+1)
     K3(kn:(kn+5)) = stateX{k}(7:12);
     K3((kn+6):(kn+11)) = K(kk:(kk+5));
 end 
+
+% Runge-Kutta step 4
+% Again, solving for rate of change for each state. This time the
+% calculation takes place at the endpoint of the interval and uses the
+% first-order estimate of the states at this point (systemStates + K4*dt).
 
 for k = 2:(links+1)
     Te{k} = getTensions(stateX{2}(1),stateX{2}(2),stateX{2}(3),stateX{2}(4),stateX{2}(5),stateX{2}(6),stateX{2}(7),stateX{2}(8),stateX{2}(9),stateX{2}(10),stateX{2}(11),stateX{2}(12), ...
@@ -116,6 +155,12 @@ for k = 2:(links+1)
     stateX{k} = stateX{k} + (K3(n:(11+n))*dt);
 end
 
+% Duct_accel solves for the six accelerations for X, Y, Z and orientation
+% variables (Tau, Gamma, Phi). Since the velocities are already encoded in
+% the state information (final six states of 12-state vector), K3 is
+% subsequently built from the last six states of the state vector and these
+% six acceleration components.
+
 K = duct_accel(stateX{2}(1),stateX{2}(2),stateX{2}(3),stateX{2}(4),stateX{2}(5),stateX{2}(6),stateX{2}(7),stateX{2}(8),stateX{2}(9),stateX{2}(10),stateX{2}(11),stateX{2}(12),Te{2}(1),Te{2}(2),Te{2}(3),Te{2}(4),Te{2}(5),Te{2}(6),Te{2}(7),Te{2}(8), ...
     stateX{3}(1),stateX{3}(2),stateX{3}(3),stateX{3}(4),stateX{3}(5),stateX{3}(6),stateX{3}(7),stateX{3}(8),stateX{3}(9),stateX{3}(10),stateX{3}(11),stateX{3}(12),Te{3}(1),Te{3}(2),Te{3}(3),Te{3}(4),Te{3}(5),Te{3}(6),Te{3}(7),Te{3}(8), ...
     stateX{4}(1),stateX{4}(2),stateX{4}(3),stateX{4}(4),stateX{4}(5),stateX{4}(6),stateX{4}(7),stateX{4}(8),stateX{4}(9),stateX{4}(10),stateX{4}(11),stateX{4}(12),Te{4}(1),Te{4}(2),Te{4}(3),Te{4}(4),Te{4}(5),Te{4}(6),Te{4}(7),Te{4}(8));
@@ -128,8 +173,10 @@ for k = 2:(links+1)
     K4((kn+6):(kn+11)) = K(kk:(kk+5));
 end 
 
+% Weighting approximations to determine system states at {t+1}.
 combinedState = initial_states + dt/6*(K1+2*K2+2*K3+K4)';
 
+% Add Gaussian noise, if desired.
 if (noise)
     for k = 1:links
         systemStates(k, 1) = combinedState(12*(k-1)+1) + 0.001*randn(1); % x
@@ -161,6 +208,5 @@ else
         systemStates(k, 12) = combinedState(12*(k-1)+12);
     end
 end
-
 
 end
