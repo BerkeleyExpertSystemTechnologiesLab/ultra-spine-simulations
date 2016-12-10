eee81cf7d28847d20171c3dcb014605e3f0d4e6c
zhu-edward <zhu.edward445@gmail.com>
2016-12-10 13:32:09 -0800

    Update getTensions.m, MPC still a work in progress

--- kinematics/2D-inv-kinematics/getTensions.m
+++ kinematics/2D-inv-kinematics/getTensions.m
@@ -27,9 +27,9 @@ w = sqrt(ll^2-(h/2)^2); % m, width from center of tetra
 
 % Mass and force parameters
 g = spineParameters.g; % m/s^2, acceleration due to gravity
-m = spineParameters.m; % kg/node
-M = m*4; % kg/tetra
-springConstant = spineParameters.k;
+% m = spineParameters.m; % kg/node
+M = spineParameters.total_m; % kg/tetra
+springConstant = spineParameters.k_vert;
 
 %% Connectivity Matrix
 % See H.-J. Schek's "The Force Density Method for Form Finding and
