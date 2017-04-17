b13f29ccf3624f61c3a2fe95d5a9545244783032
Drew Sabelhaus <apsabelhaus@berkeley.edu>
2017-04-16 17:32:16 -0700

    Added nice graphs for the inputs too. To-Do: make the plots the right size for a single slide in a presentation (if possible.)

--- control/mpc_2d/analysis/mpc_error_analysis_2D.m
+++ control/mpc_2d/analysis/mpc_error_analysis_2D.m
@@ -6,7 +6,7 @@
 % This script ASSUMES that the following variables are in the workspace:
 % xi_cl, xi_traj, u_cl, u_traj, opt_params
 
-%% Modeling things after the mpc_error_analysis 3D script,
+% Modeling things after the mpc_error_analysis 3D script,
 
 fontsize = 12;
 % Calculate the time in seconds at each point.
@@ -16,6 +16,9 @@ dt = opt_params.dt;
 t = dt : dt : num_pts*dt;
 % Adjust time so that everything is in milliseconds. This makes things clearer.
 t = t * 1000;
+
+%% States plot:
+
 % A good figure window setup is 'Position',[100,100,500,300].
 
 % Plot the XZG errors on one plot.
@@ -66,4 +69,69 @@ plot(t, xi_cl(3, 1:num_pts)*180/pi, 'g.-', 'LineWidth', 1.5 , 'markersize', 10);
 ylabel('\theta (deg)');
 % Put the time label below the bottom graph.
 xlabel('time (msec)');
+
+%% Inputs plot.
+
+% Plot the XZG errors on one plot.
+u_handle = figure;
+hold on;
+set(gca,'FontSize',fontsize);
+set(u_handle,'Position',[100,100,500,500]);
+
+% For the first subplot (u1), ...
+subplot_handle = subplot(4, 1, 1);
+hold on;
+set(gca,'FontSize',fontsize);
+% Plot input 1 length, adjusting the distances to cm
+% Reference is
+plot(t, u_traj(1, 1:num_pts)*100, 'b.-', 'LineWidth', 1.5 , 'markersize', 15);
+% Closed-loop behavior is
+plot(t, u_cl(1, 1:num_pts)*100, 'g.-', 'LineWidth', 1.5 , 'markersize', 10);
+% Labels
+title('Input Tracking with Inv. Kin. Inputs');
+ylabel('U1 (cm)');
+%xlabel('time (msec)');
+% Make the legend
+legend('Reference', 'Result', 'Location', 'Best');
+
+% For the second subplot (u2), ...
+subplot_handle = subplot(4, 1, 2);
+hold on;
+set(gca,'FontSize',fontsize);
+% Plot u2, adjusting the distances to cm
+% Reference is
+plot(t, u_traj(2, 1:num_pts)*100, 'b.-', 'LineWidth', 1.5 , 'markersize', 15);
+% Closed-loop behavior is
+plot(t, u_cl(2, 1:num_pts)*100, 'g.-', 'LineWidth', 1.5 , 'markersize', 10);
+% Labels
+ylabel('U2 (cm)');
+%xlabel('time (msec)');
+
+% For the third subplot (u3), ...
+subplot_handle = subplot(4, 1, 3);
+hold on;
+set(gca,'FontSize',fontsize);
+% Plot u3, adjusting the distances to cm
+% Reference is
+plot(t, u_traj(3, 1:num_pts)*100, 'b.-', 'LineWidth', 1.5 , 'markersize', 15);
+% Closed-loop behavior is
+plot(t, u_cl(3, 1:num_pts)*100, 'g.-', 'LineWidth', 1.5 , 'markersize', 10);
+% Labels
+ylabel('U3 (cm)');
+%xlabel('time (msec)');
+
+% For the fourth subplot (u4), ...
+subplot_handle = subplot(4, 1, 4);
+hold on;
+set(gca,'FontSize',fontsize);
+% Plot u4
+% Reference is
+plot(t, u_traj(3, 1:num_pts)*100, 'b.-', 'LineWidth', 1.5 , 'markersize', 15);
+% Closed-loop behavior is
+plot(t, u_cl(3, 1:num_pts)*100, 'g.-', 'LineWidth', 1.5 , 'markersize', 10);
+% Labels
+ylabel('U4 (cm)');
+% Put the time label below the bottom graph.
+xlabel('time (msec)');
+
 
